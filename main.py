fc5d375b3827ddd43316ae5d9300fb8fc4220d79
Luukas Lehtinen <lukituubi@gmail.com>
2023-01-26 14:00:11 +0200

    Store path and parameters in self

--- main.py
+++ main.py
@@ -1,23 +1,19 @@
 class LinkIdentifier:
     def __init__(self, URI: str):
         self.URI = URI
+        self.path = None
+        self.parameters = None
 
     def validate_uri(self):
-        URI_parts = {}
         scheme_split = self.URI.split(":")
 
-        URI_parts["scheme"] = scheme_split[0]
-        URI_parts["path"] = scheme_split[1].split("?")[0][2:]
-        URI_parts["parameters"] = dict([parameter.split("=") for parameter in self.URI.split("?")[1].split("&")])
+        self.path = scheme_split[1].split("?")[0][2:]
+        self.parameters = dict([parameter.split("=") for parameter in self.URI.split("?")[1].split("&")])
 
-        print("URI parts:", URI_parts)
-
-        if URI_parts["scheme"] != "visma-identity":
+        if scheme_split[0] != "visma-identity":
             return False
-
-        if not self.validate_parameters(URI_parts["path"], URI_parts["parameters"]):
+        if not self.validate_parameters(self.path, self.parameters):
             return False
-
         return True
 
     def validate_parameters(self, path, parameters):
